231f42dc1916c0cad7b230cfb253f516cc660fa1
Neto Vel <neto.vel.arci2998@gmail.com>
2025-03-12 21:56:54 -0700

    Maqueta de Inicio listo

--- src/main/java/dgtic/core/controller/InicioController.java
+++ src/main/java/dgtic/core/controller/InicioController.java
@@ -3,6 +3,7 @@ package dgtic.core.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,20 +12,11 @@ public class InicioController {
     @Value("${mensaje.application}")
     private String valor;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String inicio(){
-        return "inicio";
-    }
-
-    @RequestMapping(value = "modelo", method = RequestMethod.GET)
-    public String lenguajeExpresivo(Model model){
-        model.addAttribute("mensaje", "Diplomado de Spring");
-        return "inicio";
-    }
-
-    @RequestMapping(value = "propiedades", method = RequestMethod.GET)
-    public String propiedades(Model model){
+    @GetMapping(value = "/")
+    public String inicio(Model model){
         model.addAttribute("valor", valor);
+        model.addAttribute("contenido", "En esta página habrá un login.");
+        model.addAttribute("contenido2", "Esta página cambiará en 10 seg");
         return "inicio";
     }
 
