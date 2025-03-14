ec970dc5246160fb9b561e5e90a9383dd69d5a58
Neto Vel <neto.vel.arci2998@gmail.com>
2025-03-13 20:29:15 -0700

    Basic views created. It's needed add them a description yet

--- src/main/java/dgtic/core/controller/ManageController.java
+++ src/main/java/dgtic/core/controller/ManageController.java
@@ -2,54 +2,57 @@ package dgtic.core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value = "user")
-public class PrincipalController {
-    @GetMapping("realizar-venta")
-    public String getRealizarVVenta(Model modelo){
-        modelo.addAttribute("contenido", "Realizar Venta");
+@RequestMapping(value = "user/manager")
+public class ManageController {
+    @GetMapping("buscar-libro")
+    public String getBuscarLibro(Model modelo){
+        modelo.addAttribute("contenido", "Buscar libros");
         modelo.addAttribute("description",
                 "En esta página se verán las opciones para realizar una venta." +
                         " Se tendrán que desplejar los libros junto con sus respectivos datos." +
                         " Se incluirá un botón para confirmar venta");
-        return "principal/venta";
+        return "principal/libros";
     }
 
-    @GetMapping("historial-ventas")
-    public String getHistorialVentas(Model modelo){
-        modelo.addAttribute("contenido", "Historial de las ventas");
+    @GetMapping("gestionar-libro")
+    public String getGestionarLibro(Model modelo){
+        modelo.addAttribute("contenido", "Gestionar Libros");
         modelo.addAttribute("description",
                 "Se verá una tabla mostrando los libros vendidos, " +
                         "cuantos se vendieron y se inclurán las opciones para generar los reportes de las ventas");
-        return "principal/historial";
+        return "principal/gestion";
     }
 
-    @GetMapping("inventario")
-    public String getInventario(Model modelo){
-        modelo.addAttribute("contenido", "Inventario");
+    @GetMapping("gestionar-autor")
+    public String getGestionarAutor(Model modelo){
+        modelo.addAttribute("contenido", "Gestionar Autores");
         modelo.addAttribute("description",
                 "Se verá una tabla mostrando los libros vendidos, " +
                         "cuantos se vendieron y se inclurán las opciones para generar los reportes de las ventas");
-        return "principal/inventario";
+        return "principal/gestion";
     }
 
-    @GetMapping("sucursal")
-    public String getSucursal(Model modelo){
-        modelo.addAttribute("contenido", "Sucursal");
+    @GetMapping("gestionar-editoriales")
+    public String getGestionarEditorial(Model modelo){
+        modelo.addAttribute("contenido", "Gestionar Editoriales");
         modelo.addAttribute("description",
                 "Se verá una tabla mostrando los libros vendidos, " +
                         "cuantos se vendieron y se inclurán las opciones para generar los reportes de las ventas");
-        return "principal/inventario";
+        return "principal/gestion";
     }
 
-    @GetMapping("usuarios")
-    public String getUsuarios(Model modelo){
-        modelo.addAttribute("contenido", "Gestionar Usuarios");
+    @GetMapping("gestionar-clasificaciones")
+    public String getGestionarClasificacion(Model modelo){
+        modelo.addAttribute("contenido", "Gestionar Clasificaciones");
         modelo.addAttribute("description",
                 "Se verá una tabla mostrando los libros vendidos, " +
                         "cuantos se vendieron y se inclurán las opciones para generar los reportes de las ventas");
-        return "principal/usuarios";
+        return "principal/gestion";
     }
 }
